c7b1d0a0ea102b44b984cbb1660cc8a2d499d536
YrimuhMburu <105209101+YrimuhMburu@users.noreply.github.com>
2022-07-01 17:09:46 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   //let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
