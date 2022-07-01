f0ab6ea26ce236cbf8125e2429db046b6d2c14ef
YrimuhMburu <105209101+YrimuhMburu@users.noreply.github.com>
2022-07-01 13:29:21 +0300

    forecast JS added

--- src/script.js
+++ src/script.js
@@ -30,6 +30,27 @@ function formatDay(timestamp) {
 
   return days[day];
 }
+function displayForecast(){
+  let forecastElement=document.querySelector("forecast");
+  let forecastHTML=`<div class="row
+  ">`;
+  let day=["Sat","Sun","Mon","Tue","Wed"];
+  day.forEach(function(day){
+ forecastHTML=forecastHTML+'
+             
+<div class="col-2">
+<div class="weather-forecast-date">${day}</div>
+ <img src="http://openweathermap.org/img/wn/50d@2x.png" alt ="" width="42"/>
+<div class="weather-forecast-temperatures">
+<span class="weather-forecast-temperature-max">18°</span>
+<span class="weather-forecast-temperature-min">12°</span>
+</div>
+</div>
+`;
+});
+forecastHTML=forecastHTML+`</div>`;
+forecastElement.innerHTML=forecastHTML;
+}
 
 function displayWeatherCondition(response) {
   console.log(response.data);
