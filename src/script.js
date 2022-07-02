fabad2112a2ca08f75eec36347928d5ed6cb92c4
YrimuhMburu <105209101+YrimuhMburu@users.noreply.github.com>
2022-07-02 22:37:19 +0300

    updated 5 days in JS

--- src/script.js
+++ src/script.js
@@ -24,23 +24,23 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(response) {
-  let forecast = response.data.daily;
+function displayForecast() {
+  //let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  //let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
 
   let forecastHTML = `<div class="row">`;
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
 
-  forecast.forEach(function (forecastDay) {
+  days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
- <div class="col-2">
-<div class="weather-forecast-date">${forecastDay.dt}</div>
- <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt ="" width="42"/>
+  <div class="col-2">
+<div class="weather-forecast-date">${day}</div>
+ <img src="http://openweathermap.org/img/wn/50d@2x.png" alt ="" width="42"/>
 <div class="weather-forecast-temperatures">
-<span class="weather-forecast-temperature-max">${forecastDay.temp.max}°</span>
-<span class="weather-forecast-temperature-min">${forecastDay.temp.min}°</span>
+<span class="weather-forecast-temperature-max">18°</span>
+<span class="weather-forecast-temperature-min">12°</span>
 </div>
 </div>
 `;
@@ -48,12 +48,12 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "f91d45cb5615a81db62d90c4d20f6b10";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
+//function getForecast(coordinates) {
+// console.log(coordinates);
+// let apiKey = "f91d45cb5615a81db62d90c4d20f6b10";
+// let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKey}&units=metric`;
+/// axios.get(apiUrl).then(displayForecast);
+//}
 
 function displayTemperature(response) {
   console.log(response.data);
@@ -79,7 +79,7 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  getForecast(response.data.coord);
+  //getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -100,17 +100,17 @@ function displayFahrenheitTemperature(event) {
 
   let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
 
-  temperatureElement.innerHTML = Math.Round(fahrenheiTemperature);
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.Round(celsiusTemperature);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let celsiusTemperature = null;
 
-let form = document.querySelector(".search-form");
+let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
@@ -118,3 +118,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 search("Nairobi");
+displayForecast();
